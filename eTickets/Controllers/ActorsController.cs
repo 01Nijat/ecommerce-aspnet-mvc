c4581f63b8cba54ac3b10490136143178bb90c1d
01Nijat <mr.nijat.asgarov@gmail.com>
2023-01-31 17:18:53 +0400

    complete

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -17,7 +17,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             
-            var data= await _service.GetAll();
+            var data= await _service.GetAllAsync();
             return View(data);
         }
         //Get Actor Create
@@ -32,8 +32,15 @@ namespace eTickets.Controllers
             {
                 return View(actor);
             }
-            _service.Add(actor);
+           await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
+        }
+        // Get actors detail/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails=await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("empty");          
+                return View(actorDetails);           
         }
     }
 }
