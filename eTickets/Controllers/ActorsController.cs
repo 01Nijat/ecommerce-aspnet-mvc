ba43364ba4cd3b9104715e12d2921027b0d53d87
01Nijat <mr.nijat.asgarov@gmail.com>
2023-02-01 19:16:05 +0400

    Complete

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -39,8 +39,25 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Details(int id)
         {
             var actorDetails=await _service.GetByIdAsync(id);
-            if (actorDetails == null) return View("empty");          
+            if (actorDetails == null) return View("Not Found");          
                 return View(actorDetails);           
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("Not Found");
+            return View(actorDetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,[Bind("Id, ProfilePictureURL, FullName, Bio")] Actor actor)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id,actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -34,9 +34,12 @@ namespace eTickets.Data.Services
             return result;
         }
 
-        public Actor Update(int id, Actor newActor)
+        public async Task<Actor> UpdateAsync(int id, Actor newActor)
         {
-            throw new NotImplementedException();
+            //newActor.Id = id;
+            _context.Update(newActor);
+            await _context.SaveChangesAsync();
+            return newActor;
         }
     }
 }
