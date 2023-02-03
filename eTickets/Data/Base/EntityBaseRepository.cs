66b914be7da9899906495a4babcfe87c6230c64a
01Nijat <mr.nijat.asgarov@gmail.com>
2023-02-04 00:22:47 +0400

    complete

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T>: IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context;
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
