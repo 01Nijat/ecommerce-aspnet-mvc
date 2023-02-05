1d82075f0786d5cda4240cb508b9010f34e3ae8d
01Nijat <mr.nijat.asgarov@gmail.com>
2023-02-05 19:51:06 +0400

    complete

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace eTickets.Data.Base
 {
@@ -12,9 +13,11 @@ namespace eTickets.Data.Base
         public async Task AddAsync(T entity)=> await _context.Set<T>().AddAsync(entity);
        
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity= await _context.Set<T>().FirstOrDefaultAsync(n => n.id == id);
+            EntityEntry entityentry = _context.Entry<T>(entity);
+            entityentry.State = EntityState.Deleted;
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()=> await _context.Set<T>().ToListAsync();
@@ -24,9 +27,10 @@ namespace eTickets.Data.Base
         public async Task<T> GetByIdAsync(int id)=> await _context.Set<T>().FirstOrDefaultAsync(n => n.id == id);
             
             
-        public Task<T> UpdateAsync(int id, T entity)
+        public async Task UpdateAsync(int id, T entity)
         {
-            throw new NotImplementedException();
+            EntityEntry entityentry = _context.Entry<T>(entity);
+            entityentry.State= EntityState.Modified;
         }
     }
 }
